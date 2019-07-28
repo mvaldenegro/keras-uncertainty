98955e7187fd49f8def7d8518c31ffe662af96ed
Matias Valdenegro <matias.valdenegro@gmail.com>
2019-07-29 00:27:40 +0200

    Implement NLL metric in numpy.

--- keras_uncertainty/utils/numpy_metrics.py
+++ keras_uncertainty/utils/numpy_metrics.py
@@ -5,8 +5,17 @@ import numpy as np
 def accuracy(y_true, y_pred):
     return np.mean(y_true == y_pred)
 
-def negative_log_likelihood(y_true, y_pred):
-    return 0
+EPSILON = 1e-7
+
+def numpy_negative_log_likelihood(y_true, y_pred):
+    """
+        Negative log-likelihood or negative log-probability loss/metric.
+        Reference: Evaluating Predictive Uncertainty Challenge, Quiñonero-Candela et al, 2006.
+        It sums over classes: log(y_pred) for true class and log(1.0 - pred) for not true class, and then takes average across samples.
+    """
+    y_pred = np.clip(y_pred, EPSILON, 1.0 - EPSILON)
+
+    return -np.mean(np.sum(y_true * np.log(y_pred) + (1.0 - y_true) * np.log(1.0 - y_pred), axis=-1), axis=-1)
 
 def entropy(y_true, y_pred):
     return 0
