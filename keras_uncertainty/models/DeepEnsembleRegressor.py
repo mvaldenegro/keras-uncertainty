ce91f82a0dfd19c3559ca528be017a1fd18ce7a0
Matias Valdenegro <matias.valdenegro@gmail.com>
2019-10-29 16:48:53 +0100

    Correct implementation.

--- keras_uncertainty/models/DeepEnsembleRegressor.py
+++ keras_uncertainty/models/DeepEnsembleRegressor.py
@@ -3,24 +3,34 @@ import keras
 
 import keras.backend as K
 
-def deep_ensemble_regression_nll_loss(y_true, y_pred):
-    mu = y_pred[:, 0]
-    sigma_sq = y_pred[:, 1] + K.epsilon()
+def deep_ensemble_regression_nll_loss(sigma_sq, epsilon = 1e-6):
+    """
+        Regression loss for a Deep Ensemble, using the negative log-likelihood loss.
+        This function returns a keras regression loss, given a symbolic tensor for the sigma square output of the model.
+        The training model should return the mean, while the testing/prediction model should return the mean and variance.4
+    """
+    def nll_loss(y_true, y_pred):
+        return 0.5 * K.mean(K.log(sigma_sq + epsilon) + K.square(y_true - y_pred) / (sigma_sq + epsilon))
 
-    return 0.5 * K.mean(K.log(sigma_sq) + K.square(y_true - mu) / sigma_sq)
+    return nll_loss
 
 class DeepEnsembleRegressor:
     """
         Implementation of a Deep Ensemble for regression.
-        Assumes that a model outputs at least a 2D vector, where the first feature dimension is the mean, and the second feature dimension is the variance squared.
+        Uses two models
     """
     def __init__(self, model_fn, num_estimators):
         """
             Builds a Deep Ensemble given a function to make model instances, and the number of estimators.
+
+            For training it uses a model that only outputs the mean, while the loss uses both the mean and variance produced by the model.
+            For testing, a model that shares weights with the training model is used, but the testing model outputs both mean and variance. The final
+            prediction is made with a mixture of gaussians, where each gaussian is one trained model instance.
         """
         self.model_fn = model_fn
         self.num_estimators = num_estimators
-        self.estimators = [None] * num_estimators
+        self.train_estimators = [None] * num_estimators
+        self.test_estimators = [None] * num_estimators
 
     def fit(self, X, y, epochs=10, batch_size=32, **kwargs):
         """
@@ -28,8 +38,20 @@ class DeepEnsembleRegressor:
         """
 
         for i in range(self.num_estimators):
-            self.estimators[i] = self.model_fn()
-            self.estimators[i].fit(X, y, epochs=epochs, batch_size=batch_size, **kwargs)
+            models = self.model_fn()
+
+            if type(models) is not tuple:
+                raise ValueError("model_fn should return a tuple")
+
+            if len(models) is not 2:
+                raise ValueError("model_fn returned a tuple of unexpected size ({} vs 2)".format(len(models)))
+
+            train_model, test_model = models
+            train_model.fit(X, y, epochs=epochs, batch_size=batch_size, **kwargs)
+
+            self.train_estimators[i] = train_model
+            self.test_estimators[i] = test_model
+            
 
     def predict(self, X, batch_size=32):
         """
@@ -39,15 +61,16 @@ class DeepEnsembleRegressor:
         means = []
         variances = []
 
-        for estimator in self.estimators:
-            preds = estimator.predict(X, batch_size=batch_size)
-            means.append(preds[:, 0])
-            variances.append(preds[:, 1])
+        for estimator in self.test_estimators:
+            mean, var  = estimator.predict(X, batch_size=batch_size)
+            means.append(mean)
+            variances.append(var)
 
         means = np.array(means)
         variances = np.array(variances)
         
-        mixture_mean = np.mean(means, axis=0)[0]
-        mixture_var  = np.mean(variances + np.square(means)) - mixture_mean
+        mixture_mean = np.mean(means, axis=0)
+        mixture_var  = np.mean(variances + np.square(means), axis=0) - np.square(mixture_mean)
+        mixture_var[mixture_var < 0.0] = 0.0
                 
         return mixture_mean, np.sqrt(mixture_var)
