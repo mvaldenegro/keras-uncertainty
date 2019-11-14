2577562490970275fafb9c09e186a3f4c4795f74
Matias Valdenegro <matias.valdenegro@gmail.com>
2019-11-14 14:50:26 +0100

    test a bit more.

--- tests/test_mcdropout.py
+++ tests/test_mcdropout.py
@@ -4,18 +4,45 @@ import numpy as np
 from keras.layers import Dense, Dropout
 from keras.models import Sequential
 
-from keras_uncertainty.models import MCDropoutModel
+from keras_uncertainty.models import MCDropoutClassifier, MCDropoutRegressor
 
-def test_mcdropout():
+def test_mcdropout_classifier():
+    model = Sequential()
+    model.add(Dense(10, input_shape=(1,), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(3, activation="softmax"))
+
+    mc_model = MCDropoutClassifier(model)
+
+    inp = np.array([[2]])
+    probs = mc_model.predict(inp)[0]
+
+    print("One-sample prediction: {}".format(probs))
+
+    inp = np.array([[3], [4], [5]])
+    probs = mc_model.predict(inp)[0]
+
+    print("Multi-sample prediction: {}".format(probs))
+
+def test_mcdropout_regressor():
     model = Sequential()
     model.add(Dense(10, input_shape=(1,), activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1))
 
-    mc_model = MCDropoutModel(model)
+    mc_model = MCDropoutRegressor(model)
 
-    inp = np.array([[3]])
-    mean, std = mc_model.predict(inp)
+    inp = np.array([[2]])
+    probs = mc_model.predict(inp)[0]
 
-    print(mean, std)
+    print("One-sample prediction: {}".format(probs))
 
+    inp = np.array([[3], [4], [5]])
+    probs = mc_model.predict(inp)[0]
+
+    print("Multi-sample prediction: {}".format(probs))
+
+if __name__ == "__main__":
+    test_mcdropout_classifier()
+    test_mcdropout_regressor()
+     
