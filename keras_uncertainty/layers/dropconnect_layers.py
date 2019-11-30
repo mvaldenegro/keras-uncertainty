3cc55e40da6f41208ab467eb21e301d01e3a2d70
Matias Valdenegro <matias.valdenegro@gmail.com>
2019-12-01 00:41:39 +0100

    Refactor dropconnect into a base class.

--- keras_uncertainty/layers/dropconnect_layers.py
+++ keras_uncertainty/layers/dropconnect_layers.py
@@ -3,22 +3,36 @@ import keras
 from keras.layers import Dense, Conv1D, Conv2D, Conv3D
 import keras.backend as K
 
-class DropConnectDense(Dense):
-    def __init__(self, *args, **kwargs):
+class DropConnect:
+    def __init__(self, **kwargs):
         self.prob = kwargs.pop('prob', 0.5)
+        self.drop_bias = kwargs.pop("drop_bias", False)
+        self.drop_noise_shape = kwargs.pop("drop_noise_shape", None)
 
-        if 0. < self.prob < 1.:
+    @property
+    def needs_drop(self):
+        return 0.0 < self.prob < 1.0
+
+    def get_config(self):
+        config = {
+            "prob": self.prob,
+            "drop_bias": self.drop_bias,
+            "drop_noise_shape": self.drop_noise_shape
+        }
+
+        return config
+
+
+class DropConnectDense(DropConnect, Dense):
+    def __init__(self, *args, **kwargs):        
+        DropConnect.__init__(self, **kwargs)
+        Dense.__init__(self, *args, **kwargs)
+
+        if self.needs_drop:
             self.uses_learning_phase = True
 
-        if "drop_bias" in kwargs:
-            self.drop_bias = kwargs.pop("drop_bias")
-        else:
-            self.drop_bias = False
-
-        super(DropConnectDense, self).__init__(*args, **kwargs)
-
     def call(self, x, mask=None):
-        if 0. < self.prob < 1.:
+        if self.needs_drop:
             self.kernel = K.in_train_phase(K.dropout(self.kernel, self.prob), self.kernel)
 
             if self.drop_bias:
@@ -35,32 +49,27 @@ class DropConnectDense(Dense):
             
         return output
 
+    def get_config(self):
+        config_dc = DropConnect.get_config(self)
+        config_base = Dense.get_config(self)
 
-class DropConnectConv2D(Conv2D):
-    def __init__(self, *args, **kwargs):
-        self.prob = kwargs.pop('prob', 0.5)
+        return dict(list(config_dc.items()) + list(config_base.items()))
 
-        if 0. < self.prob < 1.:
+
+class DropConnectConv2D(DropConnect, Conv2D):
+    def __init__(self, *args, **kwargs):        
+        DropConnect.__init__(self, **kwargs)
+        Conv2D.__init__(self, *args, **kwargs)
+
+        if self.needs_drop:
             self.uses_learning_phase = True
 
-        if "drop_bias" in kwargs:
-            self.drop_bias = kwargs.pop("drop_bias")
-        else:
-            self.drop_bias = False
-
-        if "drop_shape" in kwargs:
-            self.drop_shape = kwargs.pop("drop_shape")
-        else:
-            self.drop_shape = None
-
-        super(DropConnectConv2D, self).__init__(*args, **kwargs)
-
     def call(self, inputs):
-        if 0. < self.prob < 1.:
-            self.kernel = K.in_train_phase(K.dropout(self.kernel, self.prob, self.drop_shape), self.kernel)
+        if self.needs_drop:
+            self.kernel = K.in_train_phase(K.dropout(self.kernel, self.prob, self.drop_noise_shape), self.kernel)
 
             if self.drop_bias:
-                self.bias = K.in_train_phase(K.dropout(self.bias, self.prob, self.drop_shape), self.bias)
+                self.bias = K.in_train_phase(K.dropout(self.bias, self.prob, self.drop_noise_shape), self.bias)
 
         outputs = K.conv2d(
                 inputs,
@@ -80,3 +89,9 @@ class DropConnectConv2D(Conv2D):
             return self.activation(outputs)
 
         return outputs
+
+    def get_config(self):
+        config_dc = DropConnect.get_config(self)
+        config_base = Conv2D.get_config(self)
+
+        return dict(list(config_dc.items()) + list(config_base.items()))
