728a272f7df7e22374532cd62e009e1df105dd27
Matias Valdenegro <matias.valdenegro@gmail.com>
2020-01-27 20:15:29 +0100

    Correct imports.

--- keras_uncertainty/utils/__init__.py
+++ keras_uncertainty/utils/__init__.py
@@ -1,3 +1,3 @@
 from .calibration import classifier_calibration_error, classifier_calibration_curve
 from .keras_metrics import entropy, negative_log_likelihood
-from .numpy_metrics import numpy_negative_log_likelihood, numpy_entropy
+from .numpy_metrics import numpy_negative_log_likelihood, numpy_entropy, numpy_classification_nll, numpy_regression_nll
