cbe2fb72f83c2fa3bf2860fe762a29aff0743a30
Matias Valdenegro <matias.valdenegro@gmail.com>
2021-08-26 22:14:05 +0200

    Bayes by Backprop Dense layer.

---
+++ keras_uncertainty/layers/bayes_backprop_layers.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+import keras
+import keras.backend as K
+from keras.layers import Layer
+from keras import activations, initializers, regularizers, constraints
+from keras.engine import InputSpec
+
+from keras_uncertainty.distributions import gaussian
+
+# Code partially based on http://krasserm.github.io/2019/03/14/bayesian-neural-networks/
+
+class BayesByBackpropDense(Layer):
+    def __init__(self,
+                 units,
+                 kl_weight,
+                 activation=None,
+                 prior_sigma_1=1.5,
+                 prior_sigma_2=0.1,
+                 prior_pi=0.5, **kwargs):
+        self.units = units
+        self.kl_weight = kl_weight
+        self.activation = activations.get(activation)
+        self.prior_sigma_1 = prior_sigma_1
+        self.prior_sigma_2 = prior_sigma_2
+        self.prior_pi_1 = prior_pi
+        self.prior_pi_2 = 1.0 - prior_pi
+        self.init_sigma = np.sqrt(self.prior_pi_1 * self.prior_sigma_1 ** 2 +
+                                  self.prior_pi_2 * self.prior_sigma_2 ** 2)
+        self.uses_learning_phase = True
+
+        super().__init__(**kwargs)
+
+    def compute_output_shape(self, input_shape):
+        return [(None, self.units)]
+
+    def build(self, input_shape):
+        self.kernel_mu = self.add_weight(name='kernel_mu',
+                                         shape=(input_shape[1], self.units),
+                                         initializer=initializers.normal(stddev=self.init_sigma),
+                                         trainable=True)
+        self.bias_mu = self.add_weight(name='bias_mu',
+                                       shape=(self.units,),
+                                       initializer=initializers.normal(stddev=self.init_sigma),
+                                       trainable=True)
+        self.kernel_rho = self.add_weight(name='kernel_rho',
+                                          shape=(input_shape[1], self.units),
+                                          initializer=initializers.constant(0.0),
+                                          trainable=True)
+        self.bias_rho = self.add_weight(name='bias_rho',
+                                        shape=(self.units,),
+                                        initializer=initializers.constant(0.0),
+                                        trainable=True)
+        super().build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        kernel_sigma = K.softplus(self.kernel_rho)
+        kernel = self.kernel_mu + kernel_sigma * K.random_normal(self.kernel_mu.shape)
+
+        bias_sigma = K.softplus(self.bias_rho)
+        bias = self.bias_mu + bias_sigma * K.random_normal(self.bias_mu.shape)
+
+        loss = self.kl_loss(kernel, self.kernel_mu, kernel_sigma) + self.kl_loss(bias, self.bias_mu, bias_sigma)
+
+        self.add_loss(K.in_train_phase(loss, 0.0))
+
+        # This always produces stochastic outputs
+        return self.activation(K.dot(inputs, kernel) + bias)
+
+    def kl_loss(self, w, mu, sigma):
+        return self.kl_weight * K.sum(gaussian.log_probability(w, mu, sigma) - self.log_prior_prob(w))
+
+    def log_prior_prob(self, w):
+        return K.log(self.prior_pi_1 * gaussian.probability(w, 0.0, self.prior_sigma_1) +
+                     self.prior_pi_2 * gaussian.probability(w, 0.0, self.prior_sigma_2))
+
+    def get_config(self):
+        config = {'units': self.units,
+                  'kl_weight': self.kl_weight,
+                  'activation': self.activation.__name__,
+                  #'bias': self.bias,
+                  'prior_sigma_1': self.prior_sigma_1,
+                  'prior_sigma_2': self.prior_sigma_2,
+                  'prior_pi_1': self.prior_pi_1}
+        base_config = super(BayesByBackpropDense, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
