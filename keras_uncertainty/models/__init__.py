fef69531265ae2bef28e5534009b24302e098d00
Matias Valdenegro <matias.valdenegro@gmail.com>
2021-08-26 22:12:13 +0200

    Expose imports.

--- keras_uncertainty/models/__init__.py
+++ keras_uncertainty/models/__init__.py
@@ -6,3 +6,7 @@ from .DeepSubEnsembleClassifier import DeepSubEnsembleClassifier
 from .DeepSubEnsembleRegressor import DeepSubEnsembleRegressor
 
 from .SimpleEnsemble import SimpleEnsemble
+
+from .gradient_confidence import GradientClassificationConfidence
+
+from .stochastic_model import StochasticClassifier, StochasticRegressor
