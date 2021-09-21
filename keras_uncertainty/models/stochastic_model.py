1c4930d056b9ea8cde0221ea03e5901727dd0ba3
Matias Valdenegro <matias.valdenegro@gmail.com>
2021-09-21 19:12:57 +0200

    Allow configurable number of samples.

--- keras_uncertainty/models/stochastic_model.py
+++ keras_uncertainty/models/stochastic_model.py
@@ -7,31 +7,35 @@ class StochasticModel:
         Stochastic model, requiring several forward passes to produce an estimate of the posterior predictive distribution.
         This class just wraps a keras model to enable dropout at inference time.
     """
-    def __init__(self, model):
+    def __init__(self, model, num_samples=10):
         """
             Builds a stochastic model from a keras model. The model should already be trained.
         """
         self.model = model
+        self.num_samples = num_samples
     
-    def predict_samples(self, x, num_samples=10, batch_size=32):
+    def predict_samples(self, x, num_samples=None, batch_size=32):
         """
             Performs num_samples predictions using the model, and returns the produced output samples.
         """
+
+        if num_samples is None:
+            num_samples = self.num_samples
 
         assert num_samples > 0
 
         samples = [None] * num_samples
 
         for i in range(num_samples):
-            samples[i] = self.model.predict(x, batch_size=batch_size, verbose=0)
+            samples[i] = self.model.predict(x, batch_size=1, verbose=0)
 
         return np.array(samples)
 
 class StochasticClassifier(StochasticModel):
-    def __init__(self, model):
-        super().__init__(model)
+    def __init__(self, model, num_samples=10):
+        super().__init__(model, num_samples)
 
-    def predict(self, inp, num_samples=10, batch_size=32):
+    def predict(self, inp, num_samples=None, batch_size=32):
         """
             Performs a prediction given input inp using MC Dropout, and returns the averaged probabilities of model output.
         """
@@ -42,10 +46,10 @@ class StochasticClassifier(StochasticModel):
         return mean_probs
 
 class  StochasticRegressor(StochasticModel):
-    def __init__(self, model):
-        super().__init__(model)
+    def __init__(self, model, num_samples=10):
+        super().__init__(model, num_samples)
 
-    def predict(self, inp, num_samples=10, batch_size=32, output_scaler=None):
+    def predict(self, inp, num_samples=None, batch_size=32, output_scaler=None):
         """
             Performs a prediction  given input inp using MC Dropout, and returns the mean and standard deviation of the model output.
         """
