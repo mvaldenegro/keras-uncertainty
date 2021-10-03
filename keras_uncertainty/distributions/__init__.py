fb23fff78cdf5c8e4852491ab3ce765ba938e2f5
Matias Valdenegro <matias.valdenegro@gmail.com>
2021-10-04 00:38:15 +0200

    Add rademacher distribution sampling.

--- keras_uncertainty/distributions/__init__.py
+++ keras_uncertainty/distributions/__init__.py
@@ -1,1 +1,2 @@
 from . import gaussian
+from . import rademacher
---
+++ keras_uncertainty/distributions/rademacher.py
@@ -0,0 +1,7 @@
+import keras
+import keras.backend as K
+
+def sample(shape):
+    samples = K.random_binomial(shape, p=0.5)
+
+    return 2.0 * samples - 1.0
