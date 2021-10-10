c844bb0848ab455ad305e5f03999637a6a218424
Matias Valdenegro <matias.valdenegro@gmail.com>
2021-10-10 15:54:23 +0200

    Expose these methods.

--- keras_uncertainty/utils/__init__.py
+++ keras_uncertainty/utils/__init__.py
@@ -2,4 +2,4 @@ from .calibration import classifier_calibration_error, classifier_calibration_cu
 from .keras_metrics import entropy, negative_log_likelihood
 from .numpy_metrics import numpy_negative_log_likelihood, numpy_entropy, numpy_classification_nll, numpy_regression_nll
 
-from .prediction import predict_batches
+from .prediction import predict_batches, make_batches
