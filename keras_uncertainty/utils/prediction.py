2dc42adf42ed230ce990bb5bf40227586839d1b5
Matias Valdenegro <matias.valdenegro@gmail.com>
2021-10-10 15:53:02 +0200

    More changes to predict_batches.

--- keras_uncertainty/utils/prediction.py
+++ keras_uncertainty/utils/prediction.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 # To make batches from array or iterable, reference https://stackoverflow.com/a/8290508/349130
+def make_batches(iter_x, iter_y, batch_size=32):
+    l = len(iter_x)
+ 
+    for ndx in range(0, l, batch_size):
+        x = iter_x[ndx:min(ndx + batch_size, l)]
+        y = iter_y[ndx:min(ndx + batch_size, l)]
+        
+        yield x, y
+
+# To make batches from array or iterable, reference https://stackoverflow.com/a/8290508/349130
 def predict_batches(predict_fn, iterable, batch_size, progress_bar=False):
     l = len(iterable)    
 
@@ -13,14 +23,14 @@ def predict_batches(predict_fn, iterable, batch_size, progress_bar=False):
         output = [None] * l
         for ndx in tqdm(range(0, l, batch_size)):
             inp = iterable[ndx:min(ndx + batch_size, l)]
-            output[ndx:min(ndx + batch_size, l)] = predict_fn(inp)[0]
+            output[ndx:min(ndx + batch_size, l)] = predict_fn(inp)
 
         return output
     else:
         output = []
         for ndx in tqdm(range(0, l, batch_size)):
             inp = iterable[ndx:min(ndx + batch_size, l)]
-            value = predict_fn(inp)[0]
+            value = predict_fn(inp)
             output.append(value)
 
         return output
