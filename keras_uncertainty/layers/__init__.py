6cb05cb556939955064e56c68ab85cc93dfac036
Matias Valdenegro <matias.valdenegro@gmail.com>
2021-11-29 17:24:44 +0100

    Enable new Conv layers.

--- keras_uncertainty/layers/__init__.py
+++ keras_uncertainty/layers/__init__.py
@@ -1,6 +1,6 @@
 from .dropconnect_layers import DropConnectDense, DropConnectConv2D
 from .rbf_layers import RBFClassifier, add_gradient_penalty, add_l2_regularization, duq_training_loop
-from .bayes_backprop_layers import BayesByBackpropDense
-from .flipout_layers import FlipoutDense
+from .variational_layers import VariationalDense, VariationalConv1D, VariationalConv2D, VariationalConv3D
+from .flipout_layers import FlipoutDense, FlipoutConv1D, FlipoutConv2D, FlipoutConv3D
 from .stochastic_layers import SamplingSoftmax, StochasticDropout
 #from .swag_layers import SWADiagonalDense
