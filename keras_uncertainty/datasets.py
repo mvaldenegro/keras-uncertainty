f0b98edf823bc07c4fa938925f20a0544c2b0db1
Matias Valdenegro <matias.valdenegro@gmail.com>
2022-01-10 17:05:23 +0100

    Allow disentangled uncertainty for some toy datasets.

--- keras_uncertainty/datasets.py
+++ keras_uncertainty/datasets.py
@@ -38,22 +38,33 @@ def toy_regression_sinusoid(num_samples=1000, ood_samples=0):
     
     return x_train, y_train
 
-def toy_regression_monotonic_sinusoid(num_samples=500, ood_samples=0):
+def toy_regression_monotonic_sinusoid(num_samples=500, ood_samples=0, disentangle_uncertainty=False):
     """
         Returns samples from a sinusoid with increasing frequency and added heteroscendatic gaussian noise.
 
         The function is x sin(x) + e1 x + e2, where e1 and e2 are samples from a Gaussian with mean = 0 and std = 0.3.
     """
-    def fn(x):
+    def fn_truth(x):
+        return x * math.sin(x)
+
+    def fn_noise(x):
         e1, e2 = np.random.normal(0.0, 0.3), np.random.normal(0.0, 0.3)
 
-        return x * math.sin(x) + e1 * x + e2
+        return e1 * x + e2
 
-    x_train, y_train = sample_function(fn, [0.0, 10.0], size=num_samples)
+    x_train, y_train_epi = sample_function(fn_truth, [0.0, 10.0], size=num_samples)
+    x_train, y_train_ale = sample_function(fn_noise, [0.0, 10.0], size=num_samples)
 
     if ood_samples > 0:
-        x_test, y_test = sample_function(fn, [10.0, 15.0], size=ood_samples)
+        x_test, y_test_epi = sample_function(fn_truth, [10.0, 15.0], size=ood_samples)
+        x_test, y_test_ale = sample_function(fn_noise, [10.0, 15.0], size=ood_samples)
 
-        return x_train, y_train, x_test, y_test
+        if not disentangle_uncertainty:
+            return x_train, y_train_epi + y_train_ale, x_test, y_test_epi + y_test_ale
+        
+        return x_train, y_train_epi, y_train_ale, x_test, y_test_epi, y_test_ale
 
-    return x_train, y_train
+    if not disentangle_uncertainty:
+        return x_train, y_train_epi + y_train_ale
+    
+    return x_train, y_train_epi, y_train_ale
