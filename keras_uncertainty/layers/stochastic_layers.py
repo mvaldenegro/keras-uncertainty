69a07ce4663a31839042f008e1b267aa95f7d1c4
Matias Valdenegro <matias.valdenegro@gmail.com>
2022-02-14 21:23:35 +0100

    Add parameter to control variance inputs in StochasticSoftmax.

--- keras_uncertainty/layers/stochastic_layers.py
+++ keras_uncertainty/layers/stochastic_layers.py
@@ -6,10 +6,23 @@ class SamplingSoftmax(Layer):
     """
         Softmax activation with Gaussian logits. Receives mean/variance logits and computes the softmax output through sampling.
     """
-    def __init__(self, num_samples=10, temperature=1.0) -> None:
+    def __init__(self, num_samples=50, temperature=1.0, variance_type="linear_std") -> None:
+        """
+        Args:
+            num_samples: Number of samples used to compute the softmax approximation.
+                         This parameter controls the trade-off between computation and approximation quality.
+            variance_type: Assumptions made on the variance input, possible values are:
+                logit: Input is a variance logit, an exponential transformation will be applied to produce standard deviation.
+                linear_std: Input is standard deviation, no transformations are applied.
+                linear_variance: Input is variance, square root will be applied to obtain standard deviation.
+        """
         super().__init__()
+
+        assert variance_type in ["logit", "linear_std", "linear_variance"]
+
         self.num_samples = num_samples
         self.temperature = temperature
+        self.variance_type = variance_type         
 
     def compute_output_shape(self, input_shape):
         assert len(input_shape) == 2
@@ -17,11 +30,20 @@ class SamplingSoftmax(Layer):
 
         return [(None, mean_is[-1])]
 
+    def preprocess_variance_input(self, var_input):
+        if self.variance_type is "logit":
+            return K.exp(var_input)
+
+        if self.variance_type is "linear_variance":
+            return K.sqrt(var_input)
+        
+        return var_input
+
     def call(self, inputs):
         assert len(inputs) == 2, "This layer requires exactly two inputs (mean and variance logits)"
 
         logit_mean, logit_var = inputs
-        logit_std = K.sqrt(logit_var)
+        logit_std = self.preprocess_variance_input(logit_var)
         logit_shape = (K.shape(logit_mean)[0], self.num_samples, K.shape(logit_mean)[-1])
 
         logit_mean = K.expand_dims(logit_mean, axis=1)
