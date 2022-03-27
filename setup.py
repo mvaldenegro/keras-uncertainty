4c959cec2268125af2369a42f231cdadaf667df3
Matias Valdenegro <matias.valdenegro@gmail.com>
2022-03-27 22:14:08 +0200

    Add scipy dependency.

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(name='Keras-Uncertainty',
       url='https://github.com/mvaldenegro/keras-uncertainty',
       download_url='https://github.com/mvaldenegro/keras-uncertainty/releases',
       license='LGPLv3',
-      install_requires=['keras>=2.2.0', 'numpy', 'tqdm'],
+      install_requires=['keras>=2.2.0', 'numpy', 'tqdm', 'scipy'],
       packages=find_packages()
      )
