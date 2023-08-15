928db61559a19ba1f66c649d216b86624fd2291e
Matias Valdenegro <matias.valdenegro@gmail.com>
2023-08-16 00:45:47 +0200

    Finish refactoring conv dropconnect layers.

--- keras_uncertainty/layers/dropconnect_layers.py
+++ keras_uncertainty/layers/dropconnect_layers.py
@@ -1,9 +1,9 @@
 import keras_uncertainty.backend as K
 
 Dense = K.layers.Dense
-Conv1D = K.layers.Conv1D
-Conv2D = K.layers.Conv2D
-Conv3D = K.layers.Conv3D
+Layer = K.layers.Layer
+activations = K.activations
+conv_utils = K.conv_utils
 
 class DropConnect:
     def __init__(self, prob=0.5, drop_bias=False, noise_shape=None):
@@ -74,77 +74,88 @@ class DropConnectDense(DropConnect, Dense):
 
         return dict(list(config_dc.items()) + list(config_base.items()))
 
-class DropConnectConv1D(DropConnect, Conv1D):
-    def __init__(self, filters, kernel_size, prob=0.5, drop_bias=False, noise_shape=None, use_learning_phase = False, **kwargs):        
+class DropConnectConvND(DropConnect, Layer):
+    def __init__(self, rank, filters, kernel_size, strides=1, padding="valid", dilation_rate=(1, 1, 1), activation="linear", prob=0.5, drop_bias=False, noise_shape=None, **kwargs):
         DropConnect.__init__(self, prob=prob, drop_bias=drop_bias, noise_shape=noise_shape)
-        Conv1D.__init__(self, filters, kernel_size, **kwargs)
+        Layer.__init__(**kwargs)
 
-        if self.needs_drop:
-            self.uses_learning_phase = use_learning_phase
+        self.rank = rank
+        self.filters = filters
+        self.kernel_size = conv_utils.normalize_tuple(kernel_size, self.rank, "kernel_size")
+        self.strides = conv_utils.normalize_tuple(strides, rank, 'strides')
+        self.padding = conv_utils.normalize_padding(padding)
+        self.dilation_rate = conv_utils.normalize_tuple(dilation_rate, rank, 'dilation_rate')
+        self.activation = activations.get(activation)
+
+    def compute_output_shape(self, input_shape):
+        space = input_shape[1:-1]
+        new_space = []
+        for i in range(len(space)):
+            new_dim = conv_utils.conv_output_length(
+                space[i],
+                self.kernel_size[i],
+                padding=self.padding,
+                stride=self.strides[i],
+                dilation=self.dilation_rate[i])
+            new_space.append(new_dim)
+
+        return (input_shape[0],) + tuple(new_space) + (self.filters,)
 
     def build(self, input_shape):
-        Conv1D.build(self, input_shape)
+        input_channels = input_shape[-1]
+        kernel_shape = self.kernel_size + (input_channels, self.filters)
+        self.kernel = self.add_weight(name="kernel_mean", shape=kernel_shape, initializer="glorot_uniform")
 
-        if self.needs_drop:
-            dc_kernel = K.dropout(self.kernel, self.prob, self.noise_shape)
-            self.kernel = self.replace_tensor(dc_kernel, self.kernel)
+        bias_shape = (self.filters, )
+        self.bias = self.add_weight(name="bias_mean", shape=bias_shape, initializer="glorot_uniform")
 
-            if self.drop_bias:
-                dc_bias = K.dropout(self.bias, self.prob, self.noise_shape)
-                self.bias = self.replace_tensor(dc_bias, self.bias)
+    def conv(self, inputs, kernel):
+        conv_dict = {
+            1: K.conv1d,
+            2: K.conv2d,
+            3: K.conv3d
+        }
 
-    def get_config(self):
-        config_dc = DropConnect.get_config(self)
-        config_base = Conv1D.get_config(self)
+        return conv_dict[self.rank](inputs, kernel, strides=self.strides, padding=self.padding, data_format="channels_last", dilation_rate=self.dilation_rate)
 
-        return dict(list(config_dc.items()) + list(config_base.items()))
+    def call(self, inputs):
+        kernel_sample = self.sample(self.kernel)
+        output = self.conv(inputs, kernel_sample)
 
-class DropConnectConv2D(DropConnect, Conv2D):
-    def __init__(self, filters, kernel_size, prob=0.5, drop_bias=False, noise_shape=None, use_learning_phase = False, **kwargs):        
-        DropConnect.__init__(self, prob=prob, drop_bias=drop_bias, noise_shape=noise_shape)
-        Conv2D.__init__(self, filters, kernel_size, **kwargs)
+        if self.use_bias:
+            bias_sample = self.sample(self.bias, dropit=self.drop_bias)
+            output = K.bias_add(output, bias_sample, data_format="channels_last")
 
-        if self.needs_drop:
-            self.uses_learning_phase = use_learning_phase
+        if self.activation is not None:
+            output = self.activation(output)
 
-    def build(self, input_shape):
-        Conv2D.build(self, input_shape)
-
-        if self.needs_drop:
-            dc_kernel = K.dropout(self.kernel, self.prob, self.noise_shape)
-            self.kernel = self.replace_tensor(dc_kernel, self.kernel)
-
-            if self.drop_bias:
-                dc_bias = K.dropout(self.bias, self.prob, self.noise_shape)
-                self.bias = self.replace_tensor(dc_bias, self.bias)
+        return output
 
     def get_config(self):
-        config_dc = DropConnect.get_config(self)
-        config_base = Conv2D.get_config(self)
+        config = DropConnect.get_config(self)
+        config.update(Layer.get_config(self))
 
-        return dict(list(config_dc.items()) + list(config_base.items()))
+        config["rank"] = self.rank
+        config["filters"] = self.filters
+        config["kernel_size"] = self.kernel_size
+        config["strides"] = self.strides
+        config["padding"] = self.padding
+        config["dilation_rate"] = self.dilation_rate
+        config["activation"] = self.activation.__name__
 
-class DropConnectConv3D(DropConnect, Conv3D):
-    def __init__(self, filters, kernel_size, prob=0.5, drop_bias=False, noise_shape=None, use_learning_phase = False, **kwargs):
-        DropConnect.__init__(self, prob=prob, drop_bias=drop_bias, noise_shape=noise_shape)
-        Conv3D.__init__(self, filters, kernel_size, **kwargs)
+        return config
 
-        if self.needs_drop:
-            self.uses_learning_phase = use_learning_phase
+class DropConnectConv1D(DropConnectConvND):
+    def __init__(self, filters, kernel_size, strides=1, padding="valid", dilation_rate=(1, ), activation="linear", prob=0.5, drop_bias=False, noise_shape=None, **kwargs):        
+        DropConnectConvND.__init__(self, rank=1, filters=filters, kernel_size=kernel_size, strides=strides, padding=padding, dilation_rate=dilation_rate, activation=activation, 
+                                   prob=prob, drop_bias=drop_bias, noise_shape=noise_shape, **kwargs)
 
-    def build(self, input_shape):
-        Conv3D.build(self, input_shape)
+class DropConnectConv2D(DropConnectConvND):
+    def __init__(self, filters, kernel_size, strides=(1, 1), padding="valid", dilation_rate=(1, 1), activation="linear", prob=0.5, drop_bias=False, noise_shape=None, **kwargs):        
+        DropConnectConvND.__init__(self, rank=2, filters=filters, kernel_size=kernel_size, strides=strides, padding=padding, dilation_rate=dilation_rate, activation=activation, 
+                                   prob=prob, drop_bias=drop_bias, noise_shape=noise_shape, **kwargs)
 
-        if self.needs_drop:
-            dc_kernel = K.dropout(self.kernel, self.prob, self.noise_shape)
-            self.kernel = self.replace_tensor(dc_kernel, self.kernel)
-
-            if self.drop_bias:
-                dc_bias = K.dropout(self.bias, self.prob, self.noise_shape)
-                self.bias = self.replace_tensor(dc_bias, self.bias)
-
-    def get_config(self):
-        config_dc = DropConnect.get_config(self)
-        config_base = Conv3D.get_config(self)
-
-        return dict(list(config_dc.items()) + list(config_base.items()))
+class DropConnectConv3D(DropConnectConvND):
+    def __init__(self, filters, kernel_size, strides=(1, 1, 1), padding="valid", dilation_rate=(1, 1, 1), activation="linear", prob=0.5, drop_bias=False, noise_shape=None, **kwargs):        
+        DropConnectConvND.__init__(self, rank=3, filters=filters, kernel_size=kernel_size, strides=strides, padding=padding, dilation_rate=dilation_rate, activation=activation, 
+                                   prob=prob, drop_bias=drop_bias, noise_shape=noise_shape, **kwargs)
